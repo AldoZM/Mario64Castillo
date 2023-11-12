f25bce9e059379770f6ca14e2b38428397618539
Bymeng <ciudad_ernesto@hotmail.com>
2023-11-11 23:53:17 -0600

    agregada la nes bien bien

--- main.cpp
+++ main.cpp
@@ -253,7 +253,7 @@ bool Start() {
 	snes = new Model("models/SNES.fbx");
 	atari = new Model("models/Atari.fbx");
 	gameCube = new Model("models/Gamecube.fbx");
-	// nes = new Model("models/NES.fbx");
+	nes = new Model("models/NES.fbx");
 
 
 	// Cubemap
@@ -935,6 +935,41 @@ bool Update() {
 		mLightsShader->setFloat("transparency", material.transparency);
 
 		atari->Draw(*mLightsShader);
+	}
+
+
+	{
+		// Aplicamos transformaciones de proyección y cámara (si las hubiera)
+
+		mLightsShader->setMat4("projection", projection);
+		mLightsShader->setMat4("view", view);
+
+		// Aplicamos transformaciones del modelo
+		//mesa1
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(9.0f, 10.0f, 50.0f)); // translate it down so it's at the center of the scene
+		model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+		model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));	// it's a bit too big for our scene, so scale it down
+		mLightsShader->setMat4("model", model);
+
+		mLightsShader->setInt("numLights", (int)gLights.size());
+		for (size_t i = 0; i < gLights.size(); ++i) {
+			SetLightUniformVec3(mLightsShader, "Position", i, gLights[i].Position);
+			SetLightUniformVec3(mLightsShader, "Direction", i, gLights[i].Direction);
+			SetLightUniformVec4(mLightsShader, "Color", i, gLights[i].Color);
+			SetLightUniformVec4(mLightsShader, "Power", i, gLights[i].Power);
+			SetLightUniformInt(mLightsShader, "alphaIndex", i, gLights[i].alphaIndex);
+			SetLightUniformFloat(mLightsShader, "distance", i, gLights[i].distance);
+		}
+
+		mLightsShader->setVec3("eye", activeCamera->Position);
+
+		mLightsShader->setVec4("MaterialAmbientColor", material.ambient);
+		mLightsShader->setVec4("MaterialDiffuseColor", material.diffuse);
+		mLightsShader->setVec4("MaterialSpecularColor", material.specular);
+		mLightsShader->setFloat("transparency", material.transparency);
+
+		nes->Draw(*mLightsShader);
 	}
 	glUseProgram(0);
 
